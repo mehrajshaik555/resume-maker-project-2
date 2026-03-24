07872758978020005dd6047064e686f629f3fef8
Mehraj shaik <mehrajshaik555@gmail.com>
2026-03-25 00:16:35 +0530

    Initial upload - Resume Maker Project 2

---
+++ script.js
@@ -0,0 +1,9 @@
+function donate() {
+  alert("❤️ Thank you for helping animals!");
+}
+
+function scrollToDonate() {
+  document.getElementById("donate").scrollIntoView({
+    behavior: "smooth"
+  });
+}
